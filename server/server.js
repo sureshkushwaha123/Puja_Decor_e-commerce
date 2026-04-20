b078f5d72db57654a18d0b0ab088bbc30e31eda0
sureshkushwaha123 <sp.kushawaha12@gmail.com>
2026-04-20 10:24:50 +0530

    added frontend link to backend

--- server/server.js
+++ server/server.js
@@ -18,7 +18,7 @@ await connectDB();
 await connectCloudinary();
 
 //Allow multiple origins
-const allowedOrigins = ['http://localhost:5173']
+const allowedOrigins = ['http://localhost:5173', 'https://pujadecor.vercel.app']
 
 // Middleware configuration
 app.use(express.json());
